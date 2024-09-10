11f84faf63b3189a63addc57281b4a85e336bf65
unseeyou <unseeyoubestmod@gmail.com>
2024-09-10 20:50:29 +1000

    feat: add multiplier effects as item data

--- bot/economy/shop.py
+++ bot/economy/shop.py
@@ -3,11 +3,42 @@ from bot.economy.economy_objects import ShopItem as Item
 from bot.economy.pet import cat, dog
 from bot.errors import TooManyShopItemsError
 
-pet_food = Item(name="Pet food", price=5, description="Feed your pet some food if it is hungry.", emoji="🍴")
-name_tag = Item("Name Tag", 10, "Give your pet with a name with this name tag.", emoji="🏷️")
-x2_income_pot = Item("2X Income Potion", 100, "Make more money. (works 8 times)", "2x_pot", "🍸")
-x10_income_pot = Item("10X Income Potion", 500, "Make all the money. (works 8 times)", item_id="10x_pot", emoji="🍷")
-cookie = Item("Cookie", 5, "Yummy.", item_id="cookie", emoji="🍪")
+# duration = times it gets used, e.g. cookie will boost for 2 work shifts
+effects = {
+    "2x_pot": {"multiplier": 2, "duration": 8},
+    "10x_pot": {"multiplier": 10, "duration": 8},
+    "cookie": {"multiplier": 1.2, "duration": 2},
+}
+
+pet_food = Item(
+    name="Pet food",
+    price=5,
+    description="Feed your pet some food if it is hungry.",
+    emoji="🍴",
+)
+name_tag = Item(
+    "Name Tag",
+    10,
+    "Give your pet with a name with this name tag.",
+    emoji="🏷️",
+)
+x2_income_pot = Item(
+    "2X Income Potion",
+    100,
+    "Make more money. (works 8 times)",
+    "2x_pot",
+    "🍸",
+    data=effects["2x_pot"],
+)
+x10_income_pot = Item(
+    "10X Income Potion",
+    500,
+    "Make all the money. (works 8 times)",
+    item_id="10x_pot",
+    emoji="🍷",
+    data=effects["10x_pot"],
+)
+cookie = Item("Cookie", 5, "Yummy.", item_id="cookie", emoji="🍪", data=effects["cookie"])
 
 # MAX 25 ITEMS
 bot_shop = Shop(
@@ -22,13 +53,6 @@ bot_shop = Shop(
         cookie,
     ],
 )
-
-# duration = times it gets used, e.g. cookie will boost for 2 work shifts
-effects = {
-    "2x_pot": {"multiplier": 2, "duration": 8},
-    "10x_pot": {"multiplier": 10, "duration": 8},
-    "cookie": {"multiplier": 1.2, "duration": 2},
-}
 
 if len(bot_shop.items) > 25:  # noqa: PLR2004
     raise TooManyShopItemsError
